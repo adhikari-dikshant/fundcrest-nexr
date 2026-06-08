0587adca1f2b04c5e319b8e0209490f66b3018b6
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2026-06-08 17:42:24 +0530

    Update Next.js configuration to include Sass support and remove unused global styles and layout files.

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import './scss/pages/home.scss';
+
+export default function Home() {
+    return (
+        <section className="hero ">
+            <div className="container-fluid h-screen flex items-end">
+                <div className="row">
+                    <div className="col-12 col-md-12">
+                        <h1 className="mb-20 text">
+                            Transparent Finance.
+                            Trusted Growth.
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    sassOptions: {
+        additionalData: `
+        @use "./scss/variable" as *;
+        @use "./scss/mixin" as mix;
+      `,
+    },
 };
 
 export default nextConfig;
